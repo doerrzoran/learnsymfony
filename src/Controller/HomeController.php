bae9d1d782591b8cd68a215a56abc0f505d01278
doerrzoran <doerrzoran@gmail.com>
2023-02-14 10:31:27 +0100

    nouvelles experiences

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -5,6 +5,7 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Services\MyName;
 
 class HomeController extends AbstractController
 {
@@ -26,8 +27,31 @@ class HomeController extends AbstractController
     }
 
     #[Route('/my/name', name: 'my-name')]
-    public function myName()
+    public function myName(MyName $myName)
     {
-        return new Response("<h1>Zoran</h1>");
+        return new Response($myName->myName());
+    }
+
+    #[Route('/link', name: 'link')]
+    public function link()
+    {
+        return new Response('<a href="/page">cliquer ici</a>');
+    }
+
+    #[Route('/page', name: 'page')]
+    public function page()
+    {
+        return new Response('hello');
+    }
+
+    #[Route('/form', name: 'form')]
+    public function form()
+    {
+        return new Response('<form action="/message" method="POST"><input type="text" name="message"><button type="submit">cliquer ici</button></form>');
+    }
+    #[Route('/message', name: 'message')]
+    public function message()
+    {
+        return new Response($_POST["message"]);
     }
 }
